84eb8380ab65b677d2eac6a4321149c9d4b289ff
DayDream-21 <vova13158@gmail.com>
2021-12-11 15:28:00 +0300

    Climbing Stairs Solved with memoization method

---
+++ src/main/java/com/slavamashkov/problems/ClimbingStairs.java
@@ -0,0 +1,31 @@
+package com.slavamashkov.problems;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println(climbStairs(45));
+    }
+
+    private static final Map<Integer, Integer> map = new HashMap<>();
+
+    private static int climbStairs(int n) {
+        
+        if (n < 0) {
+            throw new IllegalArgumentException("n < 0");
+        } else if (n == 0 || n == 1 || n == 2) {
+            return n;
+        }
+
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+
+        int result = climbStairs(n - 1) + climbStairs(n - 2);
+
+        map.put(n, result);
+
+        return result;
+    }
+}
