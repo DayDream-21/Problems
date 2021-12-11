51c6f6e9f8a17bac431f802f824c691b6c889fa3
DayDream-21 <vova13158@gmail.com>
2021-12-11 22:40:27 +0300

    Add Binary Solved

---
+++ src/main/java/com/slavamashkov/problems/AddBinary.java
@@ -0,0 +1,42 @@
+package com.slavamashkov.problems;
+
+public class AddBinary {
+    public static void main(String[] args) {
+        String str1 = "11";
+        String str2 = "1";
+
+        System.out.println(addBinary(str1, str2));
+    }
+
+    private static String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+            }
+
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+            }
+
+            sb.append(sum % 2);
+            carry = sum / 2;
+
+            i--;
+            j--;
+        }
+
+        if (carry != 0) {
+            sb.append(carry);
+        }
+
+        return sb.reverse().toString();
+    }
+}
