5b4fd5ac4946ff83decf3f37bc3af0422b6de816
DayDream-21 <vova13158@gmail.com>
2022-01-26 23:24:23 +0300

    Fibonacci
    Refactor fast method and create memoization method

--- src/main/java/com/slavamashkov/problems/Fibonacci.java
+++ src/main/java/com/slavamashkov/problems/Fibonacci.java
@@ -1,19 +1,40 @@
 package com.slavamashkov.problems;
 
+import java.util.Arrays;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibSlow(40));
+        int f = 50;
+        long[] memo = new long[f + 1];
+        // Fill array with -1 to indicate which fib-number we already calculate
+        Arrays.fill(memo, -1);
+
+        System.out.println(fibSlow(20));
+        System.out.println(fibMemo(f, memo));
         System.out.println(fibFast(1000));
     }
 
     private static long fibSlow(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
-            return 1;
+        if (n <= 1) {
+            return n;
         } else {
             return fibSlow(n - 1) + fibSlow(n - 2);
         }
+    }
+
+    private static long fibMemo(int n, long[] memo) {
+        // Check if we have calculated n-number before
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        // If not, then we calculate it and add in array
+        if (n <= 1) {
+            return n;
+        } else {
+            memo[n] = fibMemo(n - 1, memo) + fibMemo(n - 2, memo);
+        }
+
+        return memo[n];
     }
 
     private static long fibFast(int n) {
