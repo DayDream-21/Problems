b1fe4150d6156634d6c3aac7530f98f4f6ae6332
DayDream-21 <vova13158@gmail.com>
2022-02-21 21:42:00 +0300

    Solve 169. Majority Element
    * ..Brute - time complexity O(n), space complexity O(n)
    * ..Better - time complexity O(n log n), space complexity O(n)
    * ..Fast - time complexity O(n), space complexity O(1)

---
+++ src/main/java/com/slavamashkov/problems/leetcode/easy/MajorityElement.java
@@ -0,0 +1,73 @@
+package com.slavamashkov.problems.leetcode.easy;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array {@code nums} of size {@code n}, return <i>the majority element</i>.
+ * <p>
+ * The majority element is the element that appears more than
+ * {@code ⌊n / 2⌋} times. You may assume that the majority element always
+ * exists in the array.
+ * */
+
+public class MajorityElement {
+    public static void main(String[] args) {
+        int[] nums = {2,2,1,1,1,2,2};
+        System.out.println(majorityElementBrute(nums));
+        System.out.println(majorityElementBetter(nums));
+        System.out.println(majorityElementFast(nums));
+    }
+
+    private static int majorityElementBrute(int[] nums) {
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        int majorityNumber = (int) Math.floor(n / 2);
+        int result = 0;
+
+        for (int num : nums) {
+            if (map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+                if (map.get(num) > majorityNumber) {
+                    result = num;
+                }
+            } else {
+                map.put(num, 1);
+            }
+        }
+
+        return result;
+    }
+
+    private static int majorityElementBetter(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length / 2];
+    }
+
+    // Boyer–Moore majority vote algorithm
+    private static int majorityElementFast(int[] nums) {
+        int counter = 1;
+        int candidate = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            if (counter == 0) {
+                candidate = nums[i];
+            }
+
+            if (nums[i] == candidate) {
+                counter++;
+            } else {
+                counter--;
+            }
+        }
+
+        return candidate;
+    }
+}
