d99e0f1d69315fc542da1d720c5fba610bf7c8dc
DayDream-21 <vova13158@gmail.com>
2022-03-22 20:12:13 +0300

    Solve 771. Jewels and Stones
    Time-complexity O(n + m), where
     n - length of jewels,
     m - length of stones
    Space-complexity O(m)

---
+++ src/main/java/com/slavamashkov/problems/leetcode/easy/JewelsAndStones.java
@@ -0,0 +1,40 @@
+package com.slavamashkov.problems.leetcode.easy;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * You're given strings {@code jewels} representing the types of {@code stones}
+ * that are jewels, and stones representing the stones you have. Each character
+ * in {@code stones} is a type of stone you have. You want to know how many of
+ * the stones you have are also jewels.
+ * <p>
+ * Letters are case-sensitive, so  is considered a different type of stone from {@code "A"}.
+ * */
+
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+
+        System.out.println(findJewels(jewels, stones));
+    }
+
+    protected static int findJewels(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+
+        for (int i = 0; i < jewels.length(); i++) {
+            set.add(jewels.charAt(i));
+        }
+
+        for (int i = 0; i < stones.length(); i++) {
+            if (set.contains(stones.charAt(i))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
